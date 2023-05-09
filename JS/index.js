ac6e6f04dce8af4a05254a89a1b9f4cd2972444d
Santiago_Lacera <santiagoalacera@knights.ucf.edu>
2023-05-09 17:05:02 -0400

    new funtions to scroll and hover were added

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,19 @@
 const navbar = document.querySelector('.navbar');
+const navbarLinks = document.querySelectorAll('.navbar-menu ul li a');
 const homeBtn = document.querySelector('.navbar-logo');
+const formBtn = document.querySelector('.btn2');
+const leftDecoration = document.querySelector(".form-left-decoration");
+const rightDecoration = document.querySelector(".form-right-decoration");
 const home = document.querySelector('html');
 
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
     navbar.classList.add('scrolled');
-
   } else {
     navbar.classList.remove('scrolled');
   }
 });
-
-const navbarLinks = document.querySelectorAll('.navbar-menu ul li a');
 
 navbarLinks.forEach(link => {
   link.addEventListener('click', () => {
@@ -23,5 +24,16 @@ navbarLinks.forEach(link => {
 
 homeBtn.addEventListener('click', () => {
   // Update the margin style of the div element to 0
-  home.style.margin = '0';
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+formBtn.addEventListener("mouseenter", function () {
+  leftDecoration.classList.add("active");
+  rightDecoration.classList.add("active");
+});
+
+formBtn.addEventListener("mouseleave", function () {
+  leftDecoration.classList.remove("active");
+  rightDecoration.classList.remove("active");
 });
