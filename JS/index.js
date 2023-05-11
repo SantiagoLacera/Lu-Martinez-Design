1b9064a80598acd755703d1ed5aa82f644c3fa07
Santiago_Lacera <santiagoalacera@knights.ucf.edu>
2023-05-11 14:54:24 -0400

    added comments

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,14 @@
+/* global document, window */
 const navbar = document.querySelector('.navbar');
+const navbarMenu = document.querySelector('.navbar-menu');
 const navbarLinks = document.querySelectorAll('.navbar-menu ul li a');
 const homeBtn = document.querySelector('.navbar-logo');
+const navBtn = document.querySelector('.btn1');
 const formBtn = document.querySelector('.btn2');
+const logo1 = document.querySelector('.container-image-about');
 const leftDecoration = document.querySelector(".form-left-decoration");
 const rightDecoration = document.querySelector(".form-right-decoration");
+const circle = document.querySelector('.circle');
 const home = document.querySelector('html');
 
 
