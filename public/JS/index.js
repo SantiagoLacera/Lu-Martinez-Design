986d66db7068a7623a60ea79380383ac8852f745
Santiago_Lacera <santiagoalacera@knights.ucf.edu>
2023-06-11 16:22:24 -0400

    New post

---
+++ public/JS/index.js
@@ -0,0 +1,52 @@
+/* global document, window */
+const navbar = document.querySelector('.navbar');
+const navbarMenu = document.querySelector('.navbar-menu');
+const navbarLinks = document.querySelectorAll('.navbar-menu ul li a');
+const homeBtn = document.querySelector('.navbar-logo');
+const navBtn = document.querySelector('.btn1');
+const formBtn = document.querySelector('.btn2');
+const logo1 = document.querySelector('.container-image-about');
+const leftDecoration = document.querySelector(".form-left-decoration");
+const rightDecoration = document.querySelector(".form-right-decoration");
+const circle = document.querySelector('.circle');
+const form = document.querySelector('.form-inner');
+
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.add('scrolled');
+
+  });
+});
+
+homeBtn.addEventListener('click', () => {
+  // Update the margin style of the div element to 0
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+formBtn.addEventListener("mouseenter", function () {
+  leftDecoration.classList.add("active");
+  rightDecoration.classList.add("active");
+});
+
+formBtn.addEventListener("mouseleave", function () {
+  leftDecoration.classList.remove("active");
+  rightDecoration.classList.remove("active");
+});
+
+formBtn.addEventListener("click", function () {
+  form.blur();
+  console.log("clicked");
+}
+);
+
+
